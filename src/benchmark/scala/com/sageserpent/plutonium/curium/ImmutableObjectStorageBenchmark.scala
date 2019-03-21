95d11a63175cf17ebc4f61cb422123d33dc5872e
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-03-21 15:26:30 +0000

    Rework benchmark to include the number of steps as a parameter, as this is the real driver of effort. Hoist part growth step generation out of the benchmarked code, as it takes a significant slice of time.

--- src/benchmark/scala/com/sageserpent/plutonium/curium/ImmutableObjectStorageBenchmark.scala
+++ src/benchmark/scala/com/sageserpent/plutonium/curium/ImmutableObjectStorageBenchmark.scala
@@ -1,7 +1,9 @@
 package com.sageserpent.plutonium.curium
 
-import org.scalameter.api.{Bench, Gen, exec}
 import cats.implicits._
+import org.scalameter.api.{Bench, Gen, exec}
+import org.scalameter.picklers.noPickler._
+import org.scalameter.{Parameter, Parameters}
 
 import scala.util.Random
 
@@ -9,22 +11,44 @@ object ImmutableObjectStorageBenchmark extends Bench.ForkedTime {
   import ImmutableObjectStorage._
   import ImmutableObjectStorageSpec._
 
-  val numberOfLeaves = Gen.range("Number of leaves")(10, 200, 5)
+  val numberOfLeavesGenerator: Gen[ObjectReferenceId] =
+    Gen.range("Number of leaves")(10, 200, 5)
+
+  val partGrowthStepsGenerator: Gen[Stream[PartGrowthStep]] =
+    numberOfLeavesGenerator.flatMap { numberOfLeaves =>
+      val seed = numberOfLeaves
+
+      val partGrowthSteps: scala.Stream[PartGrowthStep] =
+        partGrowthStepsLeadingToRootFork(
+          allowDuplicates = true,
+          numberOfLeavesRequired = numberOfLeaves,
+          seed = seed)
+
+      val axisName = "Number of steps"
+
+      new Gen[Stream[PartGrowthStep]] {
+        override def warmupset
+          : Iterator[scala.Stream[ImmutableObjectStorageSpec.PartGrowthStep]] =
+          Iterator(partGrowthSteps)
+        override def dataset: Iterator[Parameters] =
+          Iterator(
+            Parameters(
+              Parameter[PartGrowthStep](axisName) -> partGrowthSteps.size))
+        override def generate(
+            params: Parameters): scala.Stream[PartGrowthStep] =
+          partGrowthSteps // HACK: ignore the supplied parameters, as we are generating only one possible value.
+      }
+    }
 
   performance of "Bookings" in {
-    measure method ("storeAndRetrieve") in {
-      using(numberOfLeaves) config (exec.benchRuns -> 5, exec.jvmflags -> List(
+    measure method "storeAndRetrieve" in {
+      using(partGrowthStepsGenerator) config (exec.benchRuns -> 5, exec.jvmflags -> List(
         "-Xmx3G")) in activity
     }
   }
 
-  def activity(numberOfLeaves: Int): Unit = {
-    val seed = numberOfLeaves
-
-    val partGrowthSteps = partGrowthStepsLeadingToRootFork(
-      allowDuplicates = true,
-      numberOfLeavesRequired = numberOfLeaves,
-      seed = seed)
+  def activity(partGrowthSteps: Stream[PartGrowthStep]): Unit = {
+    val seed = partGrowthSteps.size
 
     val randomBehaviour = new Random(seed)
 
