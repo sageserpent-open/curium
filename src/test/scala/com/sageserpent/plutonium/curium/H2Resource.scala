601b5775c355e03ee6288b3a319af2db2be7944b
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-11 14:48:38 +0000

    Swap out 'WorldEfficientQuestionableBackendImplementation.QuestionableTranches' for a skeleton of 'H2Tranches'. Breaks everything, as expected.

---
+++ src/test/scala/com/sageserpent/plutonium/curium/H2Resource.scala
@@ -0,0 +1,8 @@
+package com.sageserpent.plutonium.curium
+
+import com.sageserpent.plutonium.H2Tranches
+import resource._
+
+object H2Resource {
+  val transactorResource: ManagedResource[H2Tranches.Transactor] = ???
+}
