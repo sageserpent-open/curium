4c2b29ed8bfa49d290207bb79b21e74406281023
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-13 11:08:22 +0000

    Remove unused duplicate of 'H2Tranches'.

--- src/main/scala/com/sageserpent/plutonium/curium/TranchesH2Implementation.scala
+++
@@ -1,6 +0,0 @@
-package com.sageserpent.plutonium.curium
-
-import ImmutableObjectStorage.Tranches
-
-abstract class TranchesH2Implementation[Payload]
-    extends Tranches[Any, Payload] {} // TODO - make the class concrete and decide on what type the tranche ids should take - what can H2 generate automatically?
