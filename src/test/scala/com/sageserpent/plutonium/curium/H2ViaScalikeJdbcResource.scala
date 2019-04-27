6a6ad7c5c19d1be9dc203f91692aa6decba8a83e
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-04-27 21:06:15 +0000

    Actually store H2 data on a file, not just in memory.

--- src/test/scala/com/sageserpent/plutonium/curium/H2ViaScalikeJdbcResource.scala
+++ src/test/scala/com/sageserpent/plutonium/curium/H2ViaScalikeJdbcResource.scala
@@ -1,5 +1,8 @@
 package com.sageserpent.plutonium.curium
 
+import java.io.IOException
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.{FileVisitResult, FileVisitor, Files, Path}
 import java.util.UUID
 
 import cats.effect.{IO, Resource}
@@ -7,12 +10,45 @@ import scalikejdbc.{Commons2ConnectionPool, ConnectionPool}
 
 object H2ViaScalikeJdbcResource {
   val connectionPoolResource: Resource[IO, ConnectionPool] = for {
+    databaseDirectory <- Resource.make(IO {
+      Files.createTempDirectory("h2Storage")
+    })(directory =>
+      IO {
+        Files.walkFileTree(
+          directory,
+          new FileVisitor[Path] {
+            override def preVisitDirectory(
+                dir: Path,
+                attrs: BasicFileAttributes): FileVisitResult =
+              FileVisitResult.CONTINUE
+
+            override def visitFile(
+                file: Path,
+                attrs: BasicFileAttributes): FileVisitResult = {
+              Files.delete(file)
+              FileVisitResult.CONTINUE
+            }
+
+            override def visitFileFailed(file: Path,
+                                         exc: IOException): FileVisitResult =
+              FileVisitResult.CONTINUE
+
+            override def postVisitDirectory(
+                dir: Path,
+                exc: IOException): FileVisitResult = {
+              Files.delete(dir)
+              FileVisitResult.CONTINUE
+            }
+          }
+        )
+    })
     databaseName <- Resource.liftF(IO { UUID.randomUUID().toString })
     connectionPool <- Resource.make(IO {
       Class.forName("org.h2.Driver")
-      new Commons2ConnectionPool(url = s"jdbc:h2:mem:$databaseName",
-                                 user = "automatedTestIdentity",
-                                 password = "")
+      new Commons2ConnectionPool(
+        url = s"jdbc:h2:file:${databaseDirectory.resolve(databaseName)}",
+        user = "automatedTestIdentity",
+        password = "")
     })(connectionPool => IO { connectionPool.close })
     _ <- Resource.make(
       H2ViaScalikeJdbcTranches.setupDatabaseTables(connectionPool))(_ =>
