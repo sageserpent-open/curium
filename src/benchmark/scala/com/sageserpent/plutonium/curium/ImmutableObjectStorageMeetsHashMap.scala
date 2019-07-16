2a3c34a785781ea4146cc69ef1313ad38b5077d4
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-07-16 12:23:02 +0100

    Add a simple benchmark for updating a hash map that is persisted and retrieved via 'ImmutableObjectStorage'.

---
+++ src/benchmark/scala/com/sageserpent/plutonium/curium/ImmutableObjectStorageMeetsHashMap.scala
@@ -0,0 +1,63 @@
+package com.sageserpent.plutonium.curium
+
+import cats.effect.IO
+import cats.implicits._
+import com.sageserpent.plutonium.WorldH2StorageImplementation
+import com.sageserpent.plutonium.curium.H2ViaScalikeJdbcTranchesResource.TrancheId
+import com.sageserpent.plutonium.curium.ImmutableObjectStorage.{
+  IntersessionState,
+  Session
+}
+
+import scala.collection.immutable.HashMap
+import scala.concurrent.duration.Deadline
+
+object ImmutableObjectStorageMeetsHashMap
+    extends H2ViaScalikeJdbcTranchesResource {
+  def main(args: Array[String]): Unit = {
+    tranchesResource
+      .use(tranches =>
+        IO {
+          val intersessionState = new IntersessionState[TrancheId]
+
+          val immutableObjectStorage =
+            WorldH2StorageImplementation.immutableObjectStorage
+
+          val Right(initialTrancheId: TrancheId) = {
+            val session: Session[TrancheId] =
+              immutableObjectStorage.store(HashMap.empty[Int, String])
+
+            immutableObjectStorage
+              .runToYieldTrancheId(session, intersessionState)(tranches)
+          }
+
+          var trancheId: TrancheId = initialTrancheId
+
+          val startTime = Deadline.now
+
+          for (step <- 0 until 1000000) {
+            val session: Session[TrancheId] = for {
+              map <- immutableObjectStorage.retrieve[HashMap[Int, String]](
+                trancheId)
+              mutatedHashMap = map + (step -> step.toString)
+              newTrancheId <- immutableObjectStorage.store(mutatedHashMap)
+            } yield newTrancheId
+
+            trancheId = immutableObjectStorage
+              .runToYieldTrancheId(session, intersessionState)(tranches)
+              .right
+              .get
+
+            if (step % 50 == 0) {
+              val currentTime = Deadline.now
+
+              val duration = currentTime - startTime
+
+              println(
+                s"Step: $step, duration: ${duration.toMillis} milliseconds, cumulative tranche loads: ${ImmutableObjectStorage.cumulativeTrancheLoads}, cumulative tranche load size; ${ImmutableObjectStorage.cumulativeTrancheLoadSize}, maximum tranche size for proxy: ${ImmutableObjectStorage.maximumTrancheSizeForProxy}")
+            }
+          }
+      })
+      .unsafeRunSync()
+  }
+}
