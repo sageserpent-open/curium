595ea97bd18b3b8751caf9ae83f7d3219af6337c
Gerard Murphy <g.j.murphy@sageserpent.com>
2019-12-06 19:27:31 +0000

    Fix test failure.

--- src/main/scala/com/sageserpent/curium/RocksDbTranches.scala
+++ src/main/scala/com/sageserpent/curium/RocksDbTranches.scala
@@ -9,47 +9,47 @@ import org.rocksdb.{ColumnFamilyDescriptor, RocksDB}
 import scala.util.Try
 
 object RocksDbTranches {
-  val payloadValueFamilyName = "PayloadValue"
-  val objectReferenceIdOffsetValueFamilyName = "ObjectReferenceIdOffsetValue"
-  val trancheIdValueFamilyName = "TrancheIdValue"
+  val trancheIdKeyPayloadValueColumnFamilyName = "TrancheIdKeyPayloadValue"
+  val trancheIdKeyObjectReferenceIdOffsetValueFamilyName = "TrancheIdKeyObjectReferenceIdOffsetValue"
+  val objectReferenceIdKeyTrancheIdValueColumnFamilyName = "ObjectReferenceIdKeyTrancheIdValue"
 }
 
 class RocksDbTranches(rocksDb: RocksDB) extends Tranches[Array[Byte]] {
 
   import RocksDbTranches._
 
-  val payloadValueColumnFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(payloadValueFamilyName.getBytes))
-  val objectReferenceIdOffsetValueFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(objectReferenceIdOffsetValueFamilyName.getBytes))
-  val trancheIdValueColumnFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(trancheIdValueFamilyName.getBytes))
+  val trancheIdKeyPayloadValueColumnFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(trancheIdKeyPayloadValueColumnFamilyName.getBytes))
+  val trancheIdKeyObjectReferenceIdOffsetValueFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(trancheIdKeyObjectReferenceIdOffsetValueFamilyName.getBytes))
+  val objectReferenceIdKeyTrancheIdValueColumnFamily = rocksDb.createColumnFamily(new ColumnFamilyDescriptor(objectReferenceIdKeyTrancheIdValueColumnFamilyName.getBytes))
 
   override def createTrancheInStorage(payload: Array[Byte], objectReferenceIdOffset: ObjectReferenceId, objectReferenceIds: Set[ObjectReferenceId]): EitherThrowableOr[TrancheId] =
     Try {
       val trancheId = UUID.randomUUID().toString.getBytes
 
-      rocksDb.put(payloadValueColumnFamily, trancheId, payload)
+      rocksDb.put(trancheIdKeyPayloadValueColumnFamily, trancheId, payload)
 
-      rocksDb.put(objectReferenceIdOffsetValueFamily, trancheId, Ints.toByteArray(objectReferenceIdOffset))
+      rocksDb.put(trancheIdKeyObjectReferenceIdOffsetValueFamily, trancheId, Ints.toByteArray(objectReferenceIdOffset))
 
       for (objectReferenceId <- objectReferenceIds) {
-        rocksDb.put(trancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId), trancheId)
+        rocksDb.put(objectReferenceIdKeyTrancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId), trancheId)
       }
 
       trancheId
     } toEither
 
   override def objectReferenceIdOffsetForNewTranche: EitherThrowableOr[ObjectReferenceId] = Try {
-    val iterator = rocksDb.newIterator(payloadValueColumnFamily)
+    val iterator = rocksDb.newIterator(objectReferenceIdKeyTrancheIdValueColumnFamily)
     iterator.seekToLast()
     if (iterator.isValid) 1 + Ints.fromByteArray(iterator.key()) else 0
   } toEither
 
   override def retrieveTranche(trancheId: TrancheId): EitherThrowableOr[ImmutableObjectStorage.TrancheOfData] = Try {
-    val payload = rocksDb.get(payloadValueColumnFamily, trancheId)
-    val objectReferenceIdOffset = Ints.fromByteArray(rocksDb.get(objectReferenceIdOffsetValueFamily, trancheId))
+    val payload = rocksDb.get(trancheIdKeyPayloadValueColumnFamily, trancheId)
+    val objectReferenceIdOffset = Ints.fromByteArray(rocksDb.get(trancheIdKeyObjectReferenceIdOffsetValueFamily, trancheId))
     TrancheOfData(payload, objectReferenceIdOffset)
   } toEither
 
   override def retrieveTrancheId(objectReferenceId: ObjectReferenceId): EitherThrowableOr[TrancheId] = Try {
-    rocksDb.get(trancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId))
+    rocksDb.get(objectReferenceIdKeyTrancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId))
   } toEither
 }
