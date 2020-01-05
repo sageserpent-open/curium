8c2bffd74b7266d60c76be94a0196bd4577f24fe
Gerard Murphy <g.j.murphy@sageserpent.com>
2020-01-05 16:40:27 +0000

    Reinstate periodic removal of an element from a set during a step.

--- src/benchmark/scala/com/sageserpent/curium/ImmutableObjectStorageMeetsSet.scala
+++ src/benchmark/scala/com/sageserpent/curium/ImmutableObjectStorageMeetsSet.scala
@@ -2,7 +2,10 @@ package com.sageserpent.curium
 
 import cats.effect.IO
 import cats.implicits._
+import com.sageserpent.americium.randomEnrichment._
 import com.sageserpent.curium.ImmutableObjectStorage.{IntersessionState, Session}
+
+import scala.util.Random
 
 object ImmutableObjectStorageMeetsSet extends RocksDbTranchesResource {
   type TrancheId = RocksDbTranches#TrancheId
@@ -27,6 +30,8 @@ object ImmutableObjectStorageMeetsSet extends RocksDbTranchesResource {
                 .runToYieldTrancheId(session, intersessionState)(tranches)
             }
 
+            val randomBehaviour = new Random(53278953)
+
             var trancheId: TrancheId = initialTrancheId
 
             val batchSize = 100
@@ -34,7 +39,11 @@ object ImmutableObjectStorageMeetsSet extends RocksDbTranchesResource {
             for (step <- 0 until 40000000 by batchSize) {
               val session: Session[TrancheId] = for {
                 set <- immutableObjectStorage.retrieve[Set[Int]](trancheId)
-                mutatedSet = Timer.timed("Folding over steps")((set /: (step until (batchSize + step)))(_ + _))
+                mutatedSet = Timer.timed("Folding over steps")((set /: (step until (batchSize + step)))
+                  ((set2: Set[Int], step2: Int) => (if (1 == step2 % 5) {
+                    val elementToRemove = randomBehaviour.chooseAnyNumberFromZeroToOneLessThan(step2)
+                    set2 - elementToRemove
+                  } else set2) + step2))
                 newTrancheId <- immutableObjectStorage.store(mutatedSet)
               } yield newTrancheId
 
