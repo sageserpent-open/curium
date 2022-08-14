2f5d1f773545f26e2c5d964a4a53610458994768
Gerard Murphy <gjmurphy1@icloud.com>
2022-08-14 14:50:49 +0100

    Reinstate `RocksDbTranches`, cut over to implement the latest API of `Tranches`.

--- src/main/scala/com/sageserpent/curium/RocksDbTranches.scala
+++ src/main/scala/com/sageserpent/curium/RocksDbTranches.scala
@@ -1,67 +1,199 @@
-/*
 package com.sageserpent.curium
 
-import com.google.common.primitives.{Ints, Longs}
-import com.sageserpent.curium.ImmutableObjectStorage.{EitherThrowableOr, TrancheLocalObjectReferenceId, TrancheOfData, Tranches}
-import org.rocksdb.{ColumnFamilyDescriptor, ColumnFamilyOptions, CompressionType, RocksDB}
+import com.google.common.primitives.Longs
+import com.sageserpent.curium.ImmutableObjectStorage._
+import org.rocksdb._
 
+import java.lang.{Integer => JavaInteger}
+import scala.collection.mutable
 import scala.util.Try
 
 object RocksDbTranches {
+  type AugmentedTrancheLocalObjectReferenceId = Long
+
   val trancheIdKeyPayloadValueColumnFamilyName = "TrancheIdKeyPayloadValue"
-  val trancheIdKeyObjectReferenceIdOffsetValueFamilyName = "TrancheIdKeyObjectReferenceIdOffsetValue"
-  val objectReferenceIdKeyTrancheIdValueColumnFamilyName = "ObjectReferenceIdKeyTrancheIdValue"
+  val trancheIdKeyObjectReferenceIdOffsetValueFamilyName =
+    "TrancheIdKeyObjectReferenceIdOffsetValue"
+  val objectReferenceIdKeyTrancheIdValueColumnFamilyName =
+    "ObjectReferenceIdKeyTrancheIdValue"
+  val augmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValueColumnFamilyName =
+    "AugmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValue"
 }
 
 class RocksDbTranches(rocksDb: RocksDB) extends Tranches[Long] {
 
   import RocksDbTranches._
 
-  val sharedColumnFamilyOptions = new ColumnFamilyOptions().setCompressionType(CompressionType.LZ4_COMPRESSION).setBottommostCompressionType(CompressionType.ZSTD_COMPRESSION)
-
+  val sharedColumnFamilyOptions = new ColumnFamilyOptions()
+    .setCompressionType(CompressionType.LZ4_COMPRESSION)
+    .setBottommostCompressionType(CompressionType.ZSTD_COMPRESSION)
   val trancheIdKeyPayloadValueColumnFamily =
-    rocksDb.createColumnFamily(new ColumnFamilyDescriptor(trancheIdKeyPayloadValueColumnFamilyName.getBytes, sharedColumnFamilyOptions))
+    rocksDb.createColumnFamily(
+      new ColumnFamilyDescriptor(
+        trancheIdKeyPayloadValueColumnFamilyName.getBytes,
+        sharedColumnFamilyOptions
+      )
+    )
   val trancheIdKeyObjectReferenceIdOffsetValueFamily =
-    rocksDb.createColumnFamily(new ColumnFamilyDescriptor(trancheIdKeyObjectReferenceIdOffsetValueFamilyName.getBytes, sharedColumnFamilyOptions))
+    rocksDb.createColumnFamily(
+      new ColumnFamilyDescriptor(
+        trancheIdKeyObjectReferenceIdOffsetValueFamilyName.getBytes,
+        sharedColumnFamilyOptions
+      )
+    )
   val objectReferenceIdKeyTrancheIdValueColumnFamily =
-    rocksDb.createColumnFamily(new ColumnFamilyDescriptor(objectReferenceIdKeyTrancheIdValueColumnFamilyName.getBytes, sharedColumnFamilyOptions))
+    rocksDb.createColumnFamily(
+      new ColumnFamilyDescriptor(
+        objectReferenceIdKeyTrancheIdValueColumnFamilyName.getBytes,
+        sharedColumnFamilyOptions
+      )
+    )
+  val augmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValueColumnFamily =
+    rocksDb.createColumnFamily(
+      new ColumnFamilyDescriptor(
+        augmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValueColumnFamilyName.getBytes,
+        sharedColumnFamilyOptions
+      )
+    )
 
-  override def createTrancheInStorage(payload: Array[Byte], objectReferenceIdOffset: TrancheLocalObjectReferenceId, objectReferenceIds: Range): EitherThrowableOr[TrancheId] =
+  override def createTrancheInStorage(
+      tranche: TrancheOfData,
+      objectReferenceIds: Seq[CanonicalObjectReferenceId]
+  ): EitherThrowableOr[TrancheId] =
     Try {
       val trancheId = {
         val iterator = rocksDb.newIterator(trancheIdKeyPayloadValueColumnFamily)
-        iterator.seekToLast()
-        if (iterator.isValid) 1L + Longs.fromByteArray(iterator.key()) else 0L
+        try {
+          iterator.seekToLast()
+          if (iterator.isValid) 1L + Longs.fromByteArray(iterator.key()) else 0L
+        } finally {
+          iterator.close()
+        }
       }
 
       val trancheIdKey = Longs.toByteArray(trancheId)
 
-      rocksDb.put(trancheIdKeyPayloadValueColumnFamily, trancheIdKey, payload)
+      rocksDb.put(
+        trancheIdKeyPayloadValueColumnFamily,
+        trancheIdKey,
+        tranche.payload
+      )
 
-      rocksDb.put(trancheIdKeyObjectReferenceIdOffsetValueFamily, trancheIdKey, Ints.toByteArray(objectReferenceIdOffset))
+      rocksDb.put(
+        trancheIdKeyObjectReferenceIdOffsetValueFamily,
+        trancheIdKey,
+        Longs.toByteArray(tranche.objectReferenceIdOffset)
+      )
 
       for (objectReferenceId <- objectReferenceIds) {
-        rocksDb.put(objectReferenceIdKeyTrancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId), trancheIdKey)
+        rocksDb.put(
+          objectReferenceIdKeyTrancheIdValueColumnFamily,
+          Longs.toByteArray(objectReferenceId),
+          trancheIdKey
+        )
+      }
+
+      for (
+        (trancheLocalObjectReferenceId, canonicalObjectReferenceId) <-
+          tranche.interTrancheObjectReferenceIdTranslation
+      ) {
+        val augmentedTrancheLocalObjectReferenceId
+            : AugmentedTrancheLocalObjectReferenceId =
+          augmentWith(trancheId)(trancheLocalObjectReferenceId)
+
+        rocksDb.put(
+          augmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValueColumnFamily,
+          Longs.toByteArray(augmentedTrancheLocalObjectReferenceId),
+          Longs.toByteArray(canonicalObjectReferenceId)
+        )
       }
 
       trancheId
     }.toEither
 
-  override def objectReferenceIdOffsetForNewTranche: EitherThrowableOr[TrancheLocalObjectReferenceId] = Try {
-    val iterator = rocksDb.newIterator(objectReferenceIdKeyTrancheIdValueColumnFamily)
-    iterator.seekToLast()
-    if (iterator.isValid) 1 + Ints.fromByteArray(iterator.key()) else 0
+  override def objectReferenceIdOffsetForNewTranche
+      : EitherThrowableOr[CanonicalObjectReferenceId] = Try {
+    val iterator =
+      rocksDb.newIterator(objectReferenceIdKeyTrancheIdValueColumnFamily)
+    try {
+      iterator.seekToLast()
+      if (iterator.isValid) 1L + Longs.fromByteArray(iterator.key()) else 0L
+    } finally {
+      iterator.close()
+    }
   }.toEither
 
-  override def retrieveTranche(trancheId: TrancheId): EitherThrowableOr[ImmutableObjectStorage.TrancheOfData] = Try {
+  override def retrieveTranche(
+      trancheId: TrancheId
+  ): EitherThrowableOr[ImmutableObjectStorage.TrancheOfData] = Try {
     val trancheIdKey = Longs.toByteArray(trancheId)
-    val payload = rocksDb.get(trancheIdKeyPayloadValueColumnFamily, trancheIdKey)
-    val objectReferenceIdOffset = Ints.fromByteArray(rocksDb.get(trancheIdKeyObjectReferenceIdOffsetValueFamily, trancheIdKey))
-    TrancheOfData(payload, objectReferenceIdOffset)
+    val payload =
+      rocksDb.get(trancheIdKeyPayloadValueColumnFamily, trancheIdKey)
+    val objectReferenceIdOffset = Longs.fromByteArray(
+      rocksDb.get(trancheIdKeyObjectReferenceIdOffsetValueFamily, trancheIdKey)
+    )
+
+    val interTrancheObjectReferenceIdTranslation: mutable.Map[
+      TrancheLocalObjectReferenceId,
+      CanonicalObjectReferenceId
+    ] = mutable.Map.empty
+
+    val readOptions: ReadOptions = new ReadOptions()
+
+    val lowerBound = augmentWith(trancheId)(0)
+    val upperBound = augmentWith(1L + trancheId)(0)
+
+    readOptions
+      .setIterateUpperBound(new Slice(Longs.toByteArray(upperBound)))
+
+    val iterator = rocksDb.newIterator(
+      augmentedTrancheLocalObjectReferenceIdKeyCanonicalObjectReferenceIdValueColumnFamily,
+      readOptions
+    )
+
+    try {
+      iterator.seek(Longs.toByteArray(lowerBound))
+
+      while (iterator.isValid) {
+        interTrancheObjectReferenceIdTranslation +=
+          (extractTrancheLocalObjectReferenceIdFrom(
+            Longs.fromByteArray(iterator.key())
+          ) -> Longs.fromByteArray(iterator.value()))
+
+        iterator.next()
+      }
+    } finally {
+      iterator.close()
+      readOptions.close()
+    }
+
+    TrancheOfData(
+      payload,
+      objectReferenceIdOffset,
+      interTrancheObjectReferenceIdTranslation.toMap
+    )
   }.toEither
 
-  override def retrieveTrancheId(objectReferenceId: TrancheLocalObjectReferenceId): EitherThrowableOr[TrancheId] = Try {
-    Longs.fromByteArray(rocksDb.get(objectReferenceIdKeyTrancheIdValueColumnFamily, Ints.toByteArray(objectReferenceId)))
+  private def augmentWith(trancheId: TrancheId)(
+      trancheLocalObjectReferenceId: TrancheLocalObjectReferenceId
+  ): AugmentedTrancheLocalObjectReferenceId =
+    (trancheId << JavaInteger.SIZE.toLong) + JavaInteger.toUnsignedLong(
+      trancheLocalObjectReferenceId
+    )
+
+  private def extractTrancheLocalObjectReferenceIdFrom(
+      augmentedTrancheLocalObjectReferenceId: AugmentedTrancheLocalObjectReferenceId
+  ): TrancheLocalObjectReferenceId =
+    (augmentedTrancheLocalObjectReferenceId & ((1L << JavaInteger.SIZE.toLong) - 1L)).toInt
+
+  override def retrieveTrancheId(
+      objectReferenceId: CanonicalObjectReferenceId
+  ): EitherThrowableOr[TrancheId] = Try {
+    Longs.fromByteArray(
+      rocksDb.get(
+        objectReferenceIdKeyTrancheIdValueColumnFamily,
+        Longs.toByteArray(objectReferenceId)
+      )
+    )
   }.toEither
 }
-*/
--- src/test/scala/com/sageserpent/curium/TranchesBehaviours.scala
+++ src/test/scala/com/sageserpent/curium/TranchesBehaviours.scala
@@ -21,18 +21,34 @@ object TranchesBehaviours {
   val payloads: Trials[Array[Byte]] = api.bytes.several[Array[Byte]]
 
   val objectReferenceIdOffsetSequences: Trials[Seq[Long]] =
-    api.longs(lowerBound = 0L, upperBound = 500L, shrinkageTarget = 0L).lists.map(_.distinct)
+    api
+      .longs(lowerBound = 0L, upperBound = 500L, shrinkageTarget = 0L)
+      .lists
+      .map(_.distinct)
 
-  val interTrancheObjectReferenceIdTranslations: Trials[Map[TrancheLocalObjectReferenceId, CanonicalObjectReferenceId]] = api.integers.maps(api.longs).asInstanceOf
+  val interTrancheObjectReferenceIdTranslations
+  : Trials[Map[TrancheLocalObjectReferenceId, CanonicalObjectReferenceId]] =
+    api.integers
+      .maps(api.longs)
+      .asInstanceOf[Trials[
+      Map[TrancheLocalObjectReferenceId, CanonicalObjectReferenceId]
+    ]]
 
-  val cases
-  : Trials[List[(Array[Byte], Seq[Long], Map[TrancheLocalObjectReferenceId, CanonicalObjectReferenceId])]] =
-    (payloads, objectReferenceIdOffsetSequences, interTrancheObjectReferenceIdTranslations).tupled.lists.filter(_.nonEmpty)
+  val cases: Trials[List[
+    (
+      Array[Byte],
+        Seq[Long],
+        Map[TrancheLocalObjectReferenceId, CanonicalObjectReferenceId]
+      )
+  ]] =
+    (
+      payloads,
+      objectReferenceIdOffsetSequences,
+      interTrancheObjectReferenceIdTranslations
+      ).tupled.lists.filter(_.nonEmpty)
 }
 
-trait TranchesBehaviours[TrancheId]
-  extends AnyFlatSpec
-    with Matchers {
+trait TranchesBehaviours[TrancheId] extends AnyFlatSpec with Matchers {
   this: TranchesResource[TrancheId] =>
 
   import TranchesBehaviours._
@@ -42,99 +58,127 @@ trait TranchesBehaviours[TrancheId]
     val maximumNumberOfCases = 100
 
     "creating a tranche" should "yield a unique tranche id" in
-      cases.withLimit(maximumNumberOfCases).supplyTo {
-        payloadAndCountPairs =>
-          tranchesResource
-            .use(tranches =>
-              IO {
-                val numberOfPayloads = payloadAndCountPairs.size
+      cases.withLimit(maximumNumberOfCases).supplyTo { payloadAndCountPairs =>
+        tranchesResource
+          .use(tranches =>
+            IO {
+              val numberOfPayloads = payloadAndCountPairs.size
 
-                val trancheIds = MutableSet.empty[TrancheId]
+              val trancheIds = MutableSet.empty[TrancheId]
 
-                for ((payload, offsets, interTrancheObjectReferenceIdTranslation) <- payloadAndCountPairs) {
-                  val Right(trancheId) =
-                    for {
-                      objectReferenceIdOffset <- tranches.objectReferenceIdOffsetForNewTranche
-                      objectReferenceIds: Seq[CanonicalObjectReferenceId] = offsets map (objectReferenceIdOffset + _)
-                      trancheId <- tranches.createTrancheInStorage(
-                        TrancheOfData(payload, objectReferenceIdOffset, interTrancheObjectReferenceIdTranslation), objectReferenceIds)
-                    } yield trancheId
+              for (
+                (payload, offsets, interTrancheObjectReferenceIdTranslation) <-
+                  payloadAndCountPairs
+              ) {
+                val Right(trancheId) =
+                  for {
+                    objectReferenceIdOffset <-
+                      tranches.objectReferenceIdOffsetForNewTranche
+                    objectReferenceIds: Seq[CanonicalObjectReferenceId] =
+                      offsets map (objectReferenceIdOffset + _)
+                    trancheId <- tranches.createTrancheInStorage(
+                      TrancheOfData(
+                        payload,
+                        objectReferenceIdOffset,
+                        interTrancheObjectReferenceIdTranslation
+                      ),
+                      objectReferenceIds
+                    )
+                  } yield trancheId
 
-                  trancheIds += trancheId
-                }
+                trancheIds += trancheId
+              }
 
-                trancheIds should have size numberOfPayloads
-              })
-            .unsafeRunSync
+              trancheIds should have size numberOfPayloads
+            }
+          )
+          .unsafeRunSync
       }
 
     it should "permit retrieval of that tranche id from any of the associated object reference ids" in
-      cases.withLimit(maximumNumberOfCases).supplyTo {
-        payloadAndCountPairs =>
-          tranchesResource
-            .use(tranches =>
-              IO {
-                val objectReferenceIdsByTrancheId =
-                  MutableMap.empty[TrancheId, Seq[CanonicalObjectReferenceId]]
+      cases.withLimit(maximumNumberOfCases).supplyTo { payloadAndCountPairs =>
+        tranchesResource
+          .use(tranches =>
+            IO {
+              val objectReferenceIdsByTrancheId =
+                MutableMap.empty[TrancheId, Seq[CanonicalObjectReferenceId]]
 
-                for ((payload, offsets, interTrancheObjectReferenceIdTranslation) <- payloadAndCountPairs) {
-                  val Right((trancheId, objectReferenceIds)) =
-                    for {
-                      objectReferenceIdOffset <- tranches.objectReferenceIdOffsetForNewTranche
-                      objectReferenceIds: Seq[CanonicalObjectReferenceId] = offsets map (objectReferenceIdOffset + _)
-                      trancheId <- tranches.createTrancheInStorage(
-                        TrancheOfData(payload, objectReferenceIdOffset, interTrancheObjectReferenceIdTranslation),
-                        objectReferenceIds)
-                    } yield trancheId -> objectReferenceIds
+              for (
+                (payload, offsets, interTrancheObjectReferenceIdTranslation) <-
+                  payloadAndCountPairs
+              ) {
+                val Right((trancheId, objectReferenceIds)) =
+                  for {
+                    objectReferenceIdOffset <-
+                      tranches.objectReferenceIdOffsetForNewTranche
+                    objectReferenceIds: Seq[CanonicalObjectReferenceId] =
+                      offsets map (objectReferenceIdOffset + _)
+                    trancheId <- tranches.createTrancheInStorage(
+                      TrancheOfData(
+                        payload,
+                        objectReferenceIdOffset,
+                        interTrancheObjectReferenceIdTranslation
+                      ),
+                      objectReferenceIds
+                    )
+                  } yield trancheId -> objectReferenceIds
 
-                  objectReferenceIdsByTrancheId += (trancheId -> objectReferenceIds)
-                }
+                objectReferenceIdsByTrancheId += (trancheId -> objectReferenceIds)
+              }
 
-                objectReferenceIdsByTrancheId.foreach {
-                  case (trancheId, objectReferenceIds) =>
-                    objectReferenceIds.foreach { objectReferenceId =>
-                      val Right(retrievedTrancheId) =
-                        tranches.retrieveTrancheId(objectReferenceId)
-                      retrievedTrancheId shouldBe trancheId
-                    }
-                }
-              })
-            .unsafeRunSync
+              objectReferenceIdsByTrancheId.foreach {
+                case (trancheId, objectReferenceIds) =>
+                  objectReferenceIds.foreach { objectReferenceId =>
+                    val Right(retrievedTrancheId) =
+                      tranches.retrieveTrancheId(objectReferenceId)
+                    retrievedTrancheId shouldBe trancheId
+                  }
+              }
+            }
+          )
+          .unsafeRunSync
       }
 
     "retrieving a tranche by tranche id" should "yield a tranche that corresponds to what was used to create it" in
-      cases.withLimit(maximumNumberOfCases).supplyTo {
-        payloadAndCountPairs =>
-          tranchesResource
-            .use(tranches =>
-              IO {
-                val trancheIdToExpectedTrancheMapping =
-                  MutableMap.empty[TrancheId, TrancheOfData]
+      cases.withLimit(maximumNumberOfCases).supplyTo { payloadAndCountPairs =>
+        tranchesResource
+          .use(tranches =>
+            IO {
+              val trancheIdToExpectedTrancheMapping =
+                MutableMap.empty[TrancheId, TrancheOfData]
 
-                for ((payload, offsets, interTrancheObjectReferenceIdTranslation) <- payloadAndCountPairs) {
-                  val Right((trancheId, tranche)) =
-                    for {
-                      objectReferenceIdOffset <- tranches.objectReferenceIdOffsetForNewTranche
-                      objectReferenceIds: Seq[CanonicalObjectReferenceId] = offsets map (objectReferenceIdOffset + _)
-                      tranche = TrancheOfData(payload, objectReferenceIdOffset, interTrancheObjectReferenceIdTranslation)
+              for (
+                (payload, offsets, interTrancheObjectReferenceIdTranslation) <-
+                  payloadAndCountPairs
+              ) {
+                val Right((trancheId, tranche)) =
+                  for {
+                    objectReferenceIdOffset <-
+                      tranches.objectReferenceIdOffsetForNewTranche
+                    objectReferenceIds: Seq[CanonicalObjectReferenceId] =
+                      offsets map (objectReferenceIdOffset + _)
+                    tranche = TrancheOfData(
+                      payload,
+                      objectReferenceIdOffset,
+                      interTrancheObjectReferenceIdTranslation
+                    )
 
-                      trancheId <- tranches.createTrancheInStorage(
-                        tranche,
-                        objectReferenceIds)
-                    } yield
-                      trancheId -> tranche
+                    trancheId <- tranches
+                      .createTrancheInStorage(tranche, objectReferenceIds)
+                  } yield trancheId -> tranche
 
-                  trancheIdToExpectedTrancheMapping += (trancheId -> tranche)
-                }
+                trancheIdToExpectedTrancheMapping += (trancheId -> tranche)
+              }
 
-                trancheIdToExpectedTrancheMapping.foreach {
-                  case (trancheId, expectedTranche) =>
-                    val Right(tranche) = tranches.retrieveTranche(trancheId)
+              trancheIdToExpectedTrancheMapping.foreach {
+                case (trancheId, expectedTranche) =>
+                  val Right(tranche) = tranches.retrieveTranche(trancheId)
 
-                    tranche shouldBe expectedTranche
-                }
-              })
-            .unsafeRunSync
+                  tranche shouldBe expectedTranche
+              }
+            }
+          )
+          .unsafeRunSync
       }
   }
 }
@@ -159,38 +203,36 @@ class FakeTranchesSpec
   "Fake tranches" should behave like tranchesBehaviour
 }
 
-/*
-object H2ViaScalikeJdbcTranchesResource {
-  type TrancheId = H2ViaScalikeJdbcTranches#TrancheId
-}
-
-trait H2ViaScalikeJdbcTranchesResource
-  extends TranchesResource[H2ViaScalikeJdbcTranchesResource.TrancheId]
-    with H2ViaScalikeJdbcDatabaseSetupResource {
-  override val tranchesResource
-  : Resource[IO, Tranches[H2ViaScalikeJdbcTranchesResource.TrancheId]] =
-    connectionPoolResource.map(
-      connectionPool =>
-        new H2ViaScalikeJdbcTranches(connectionPool)
-          with TranchesContracts[H2ViaScalikeJdbcTranchesResource.TrancheId])
-}
-
-class H2ViaScalikeJdbcTranchesSpec
-  extends TranchesBehaviours[H2ViaScalikeJdbcTranchesResource.TrancheId]
-    with H2ViaScalikeJdbcTranchesResource {
-  "H2 tranches" should behave like tranchesBehaviour
-}
-
+/* object H2ViaScalikeJdbcTranchesResource { type TrancheId =
+ * H2ViaScalikeJdbcTranches#TrancheId }
+ *
+ * trait H2ViaScalikeJdbcTranchesResource extends
+ * TranchesResource[H2ViaScalikeJdbcTranchesResource.TrancheId] with
+ * H2ViaScalikeJdbcDatabaseSetupResource { override val tranchesResource :
+ * Resource[IO, Tranches[H2ViaScalikeJdbcTranchesResource.TrancheId]] =
+ * connectionPoolResource.map( connectionPool => new
+ * H2ViaScalikeJdbcTranches(connectionPool) with
+ * TranchesContracts[H2ViaScalikeJdbcTranchesResource.TrancheId]) }
+ *
+ * class H2ViaScalikeJdbcTranchesSpec extends
+ * TranchesBehaviours[H2ViaScalikeJdbcTranchesResource.TrancheId] with
+ * H2ViaScalikeJdbcTranchesResource { "H2 tranches" should behave like
+ * tranchesBehaviour } */
 object RocksDbTranchesResource {
   type TrancheId = RocksDbTranches#TrancheId
 }
 
-trait RocksDbTranchesResource extends TranchesResource[RocksDbTranchesResource.TrancheId] with RocksDbResource {
-  override val tranchesResource: Resource[IO, Tranches[RocksDbTranchesResource.TrancheId]] = for {
+trait RocksDbTranchesResource
+  extends TranchesResource[RocksDbTranchesResource.TrancheId]
+    with RocksDbResource {
+  override val tranchesResource
+  : Resource[IO, Tranches[RocksDbTranchesResource.TrancheId]] = for {
     rocksDb <- rocksDbResource
   } yield new RocksDbTranches(rocksDb)
 }
 
-class RocksDbTranchesSpec extends TranchesBehaviours[RocksDbTranchesResource.TrancheId] with RocksDbTranchesResource {
+class RocksDbTranchesSpec
+  extends TranchesBehaviours[RocksDbTranchesResource.TrancheId]
+    with RocksDbTranchesResource {
   "RocksDB tranches" should behave like tranchesBehaviour
-}*/
+}
